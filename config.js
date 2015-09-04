08429ce87ce969790160372679d75f5590b4b2a7
anatolich <dimasmith@gmail.com>
2015-09-04 23:24:48 +0300

    Use host name for socket connection in production mode

--- config.js
+++ config.js
@@ -6,14 +6,12 @@ const host = process.env.SERVER_HOST || 'localhost'; // host name for server
 const port = process.env.SERVER_PORT || 8000; // server port
 const development = process.env.NODE_ENV !== 'production'; // should run in development mode
 const proxyPort = process.env.DEV_SERVER_PORT || 9000; // development proxy port (webpack dev server)
-const webSocketAddress = 'http://' + host + ':' + port;
 
 module.exports = {
   host: host,
   port: port,
   development: development,
   proxyPort: proxyPort,
-  webSocketAddress: webSocketAddress,
 
   // Directory containing photos to display.
   // If set to false no directory is scanned.
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 var webpack = require('webpack');
 var config = require('./config');
 
+const webSocketAddress = (config.development)
+  ? '"http://' + config.host + ':' + config.port + '"'
+  : 'window.location.href';
+
 module.exports = {
   context: __dirname + '/client',
   entry: './app',
@@ -11,14 +15,14 @@ module.exports = {
   devtool: 'eval',
   module: {
     loaders: [
-      { test: /\.jade$/, loader: 'jade-loader'},
-      { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader?optional=runtime'},
-      { test: /\.scss$/, loader: 'style!css!sass'},
+      {test: /\.jade$/, loader: 'jade-loader'},
+      {test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader?optional=runtime'},
+      {test: /\.scss$/, loader: 'style!css!sass'},
     ],
   },
   plugins: [
     new webpack.DefinePlugin({
-      WEBSOCKET_ADDRESS: '"' + config.webSocketAddress + '"',
+      WEBSOCKET_ADDRESS: webSocketAddress,
     }),
   ],
 };
