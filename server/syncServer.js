ff98adc4c8b1543792b5b8bfedac522754869462
anatolich <dimasmith@gmail.com>
2015-09-11 23:27:31 +0300

    Move  gallery handling out of SyncServer
    #3

--- server/syncServer.js
+++ server/syncServer.js
@@ -2,10 +2,21 @@
  * Server communication of gallery state between server and clients.
  */
 import SocketTransport from '../common/SocketTransport.js';
-import Session from './Session.js';
+import Server from '../common/SyncServer.js';
 
 export default function(io, gallery) {
   io.on('connection', (socket) => {
-    Session.start(new SocketTransport(socket), gallery);
+    let session = new Server(new SocketTransport(socket));
+
+    session.onRequestGallery(() => session.sendGallery(gallery.getState()));
+    session.onShowThumbnails(() => {
+      gallery.openThumbnails();
+      session.sendOpenThumbnails();
+    });
+
+    session.onChangePhoto((data) => {
+      gallery.openPhoto(data.index);
+      session.sendOpenPhoto(gallery.getState());
+    });
   });
 }
