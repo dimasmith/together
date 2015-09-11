c45090b8b5529f29b19bedd2ecdddfc1d822f881
anatolich <dimasmith@gmail.com>
2015-09-12 01:34:28 +0300

    Enable uglify plugin for front-end build
    Plugin was disabled by mistake
    fix #2

--- webpack.frontend.config.js
+++ webpack.frontend.config.js
@@ -42,7 +42,6 @@ export default {
       template: 'client/index.html',
       inject: 'body',
     }),
-
-    //new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
   ],
 };
